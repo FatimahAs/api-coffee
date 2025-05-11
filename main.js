0b29dedc08ee9e866e4c70c8e19e054b0c768c04
FATIMAH ASIRI <fatimahasiri@FATIMAHs-MacBook-Pro-2.local>
2025-05-11 13:03:41 +0300

    add lab2

---
+++ main.js
@@ -0,0 +1,13 @@
+let img = document.createElement("img");
+img.style.width = "300px";
+document.body.appendChild(img);
+
+function Coffee() {
+    fetch('https://corsproxy.io/?url=https://coffee.alexflipnote.dev/random.json')
+        .then(response => response.json())
+        .then(data => {
+            img.src = data.file;
+        })
+}
+Coffee();
+setInterval(Coffee, 5000);
